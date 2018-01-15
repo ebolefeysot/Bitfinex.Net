13291430decd02d7edcbce611c1145688a894542
Eric Bole-Feysot <help@romcenter.com>
2018-01-15 18:02:09 +0100

    Add console demo project

---
+++ ConsoleApp/Log.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Bitfinex.Net.Interfaces;
+
+namespace ConsoleApp
+{
+    internal class Log : ILogger
+    {
+        //todo nlog to console
+        public TextWriter TextWriter { get; set; } = new TraceTextWriter();
+
+        public LogVerbosity Level { get; set; } = LogVerbosity.Warning;
+
+        public void Write(LogVerbosity logType, string message)
+        {
+            if ((int)logType >= (int)Level)
+                TextWriter.WriteLine($"{DateTime.Now:hh:mm:ss:fff} | {logType} | {message}");
+        }
+
+        public void Warn(string message)
+        {
+            Write(LogVerbosity.Warning, message);
+        }
+
+        public void Error(string message, Exception e = null)
+        {
+            Write(LogVerbosity.Error, $"{message}\n{e?.Message}");
+        }
+
+        public void Trace(string message)
+        {
+            Write(LogVerbosity.Trace, message);
+        }
+
+        public void Debug(string message)
+        {
+            Write(LogVerbosity.Debug, message);
+        }
+    }
+
+    public enum LogVerbosity
+    {
+        Debug,
+        Warning,
+        Error,
+        Trace
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Bitfinex.Net;
+using Bitfinex.Net.Interfaces;
+using Bitfinex.Net.Objects;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger log = new Log();
+
+            var bfx = new BitfinexSocketClient(log);
+
+            //connect
+            bfx.Connect();
+
+            //subscribe
+            bfx.SubscribeToTrades("btcusd", TradesTicker);
+
+            //wait
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Ticker callback
+        /// </summary>
+        /// <param name="tick"></param>
+        private static void TradesTicker(BitfinexTradeSimple[] tick)
+        {
+            //first tick contains several data (newer first)
+            foreach (var t in tick.Reverse())
+            {
+                Console.BackgroundColor = t.Type == TypeEnum.Sell ? ConsoleColor.DarkRed : ConsoleColor.DarkGreen;
+                Console.WriteLine(string.Format($"Id: {t.Id} Date: {t.Timestamp.ToLocalTime()} Price: {t.Price}, vol: {t.Amount:+0.########;-0.########}"));
+            }
+        }
+    }
+}
